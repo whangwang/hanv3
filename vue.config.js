7a63fd6a34ebeb38bfb962f5b02d5ae1935dd9af
Han Wang <whangwang0430@gmail.com>
2025-02-12 11:08:08 +1100

    update dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/hanv3/" : "/"
+  transpileDependencies: true
 })
